adb6cf92bc27768b76ce25a79dcb1887d300d957
Esther Chong <estherchong731@gmail.com>
2023-07-23 21:40:31 -0400

    edited core

---
+++ core/customer_utils.py
@@ -0,0 +1,28 @@
+import core.utils
+
+class Customer:
+    """
+    Customer is a class that is about the customer.
+
+    """
+
+    def __init__():
+        None
+
+class Orders:
+    """
+    Orders is a class that is about the order.
+
+    """
+
+    def __init__():
+        None
+
+class Share:
+    """
+    Share is a class that is about sharing.
+
+    """
+
+    def __init__():
+        None
---
+++ core/product_utils.py
@@ -0,0 +1,28 @@
+import core.utils
+
+class Product:
+    """
+    Product is a class that shows the product.
+    
+    """
+
+    def __init__():
+        None
+
+class Rating:
+    """
+    Rating is a class that saves the user's rate of the product.
+
+    """
+
+    def __init__():
+        None
+
+class Inventory:
+    """
+    Inventory is a class that shows the inventory of the product.
+
+    """
+        
+    def __init__():
+        None
