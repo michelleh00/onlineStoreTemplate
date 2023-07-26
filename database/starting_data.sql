5ffe2ea873c006c3c2c3d46d2850edeb5be82366
michelleh00 <michelle.hamilton90@yahoo.com>
2023-07-25 21:04:37 -0400

    Added 8 inventory items to database/starting_data.sql; Added inventory images to static/images

--- database/starting_data.sql
+++ database/starting_data.sql
@@ -14,10 +14,28 @@ INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `ca
 VALUES ('Apples', 'An edible cultivation of the Malus genus.', 2.00, 100, 'static/images/apple.jpeg', 'Fruit');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Bananas', 'A long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe.', 1.00, 100, 'static/images/banana.jpeg', 'Fruit');
+VALUES ('Willy Wonka and the Chocolate Factory (1971)', "A poor but hopeful boy seeks one of the five coveted golden tickets that will send him on a tour of Willy Wonka's mysterious chocolate factory.", 19.00, 25, 'static/images/willy_wonka.jpg', 'DVD');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Mangos', 'The best fruit on the planet.', 4.00, 100, 'static/images/mango.jpeg', 'Fruit');
+VALUES ('Casino (1995)', 'In Las Vegas, two best friends - a casino executive and a mafia enforcer - compete for a gambling empire and a fast-living, fast-loving socialite.', 22.00, 40, 'static/images/casino.jpg', 'DVD');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('National Lampoons Christmas Vacation (1989)', "The Griswold family's plans for a big family Christmas predictably turn into a big disaster.", 4.00, 25, 'static/images/christmas_vacation.jpg', 'DVD');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Doctor Sleep (Hardback)', 'The now middle-aged Dan Torrance (the boy protagonist of The Shining) must save a very special twelve-year-old girl from a tribe of murderous paranormals.', 26.00, 100, 'static/images/doctor_sleep.png', 'Book');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('The Shining (1980)', 'A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.', 16.00, 30, 'static/images/shining.jpg', 'DVD');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Goodfellas (1990)', 'The story of Henry Hill and his life in the mafia, covering his relationship with his wife Karen and his mob partners Jimmy Conway and Tommy DeVito.', 18.00, 25, 'static/images/goodfellas.jpg', 'DVD');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('The Hobbit (Paperback)', "J.R.R. Tolkien's classic prelude to his Lord of the Rings trilogy. Go on an adventure with Bilbo Baggins as he finds himself roped into a quest to defeat Smaug the dragon!", 19.00, 50, 'static/images/hobbit.jpg', 'Book');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Goosebumps: Night of the Living Dummy', "Kris's twin sister has just gotten a ventriloquist's dummy and it's all anyone - their parents, their friends - seem to care about. Kris is tired of being ignored so she gets a dummy of her own. But double the dummies start to mean double the trouble...and horror.", 9.00, 100, 'static/images/goosebumps.jpg', 'Book');
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
